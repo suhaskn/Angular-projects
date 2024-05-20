206b25bd308702a8f72c4e6562b4b3abb4927353
Suhas KN <suhaskn09@gmail.com>
2024-05-20 20:08:55 +0200

    latest change in the app

--- alfadak/src/app/app.component.ts
+++ alfadak/src/app/app.component.ts
@@ -1,13 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NewsComponent } from './components/news/news.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, HomeComponent, NewsComponent, ContactComponent, AboutComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ALFADAK';
+  status: boolean = false;
+
+  onClick(value: string) {
+    // if (value === 'home') {
+    //   this.status = value === 'home' ? true : false;
+    // }
+    // if (value === 'news') {
+    //   this.status = value === 'news' ? true : false;
+    // }
+    // if (value === 'contact') {
+    //   this.status = value === 'contact' ? true : false;
+    // }
+    // if (value === 'about') {
+    //   this.status = value === 'about' ? true : false;
+    // }
+  }
 }
--- alfadak/src/app/app.routes.ts
+++ alfadak/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { NewsComponent } from './components/news/news.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: AboutComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'news', component: NewsComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: 'about', component: AboutComponent},
+];
